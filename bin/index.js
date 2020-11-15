4744423405a4cd47e840142223cbf55be1d329ee
Angus Woodman <angus@Anguss-Air-2.home>
2020-11-15 20:40:53 +0000

    add comments

--- bin/index.js
+++ bin/index.js
@@ -3,11 +3,13 @@
 const colors = require('colors');
 const convert = require("../lib/convert");
 
+// create interface for user prompts
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// promt user to enter a unit conversion
 readline.question('Please enter the conversion you would like to do. (Ex. 73 cm in m)', unit => {
   let answer = unit.split(' ')
   if (answer.length != 4) {
@@ -16,7 +18,10 @@ readline.question('Please enter the conversion you would like to do. (Ex. 73 cm 
   let amount = answer[0]
   let unit_init = answer[1]
   let unit_new = answer[3]
+
+  // run converter function from conver.js with the user's input and log the answer
   console.log(convert.converter(amount, unit_init, unit_new))
 }
+  // close the interface
   readline.close();
 });
